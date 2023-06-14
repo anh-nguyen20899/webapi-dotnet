08dbbb54af6f9ec4c2f3b2b0e4165ce467ac0dfb
anhng <anananh1999@gmail.com>
2023-06-14 21:25:28 +1000

    Create Migration for Product Table. Confirgure Connection Strings

---
+++ mywebapi/Data/MyDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using mywebapi.Models;
+
+namespace mywebapi.Data;
+
+public class MyDbContext : DbContext
+{
+    // Initilize Contructor
+    public MyDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    /*
+    DbSet
+    */
+
+    public DbSet<Product> Products {get; set;}
+
+}
