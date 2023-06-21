9f160f955f853e0084d62751e808c454b0939c0b
anhng <anananh1999@gmail.com>
2023-06-21 20:40:36 +1000

    Add Order and OrderDetails tables with Fluentapi

--- mywebapi/Data/MyDbContext.cs
+++ mywebapi/Data/MyDbContext.cs
@@ -8,18 +8,18 @@ public class MyDbContext : DbContext
     // Initilize Contructor
     public MyDbContext(DbContextOptions options) : base(options)
     {
-        
+
     }
     /*
     DbSet
     */
 
-    public DbSet<Product> Products {get; set;}
-    public DbSet<Category> Categories {get; set;}
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
 
-    public DbSet<Order> Order {get; set;}
+    public DbSet<Order> Order { get; set; }
 
-    public DbSet<OrderDetails> OrderDetails {get; set;}
+    public DbSet<OrderDetails> OrderDetails { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -33,7 +33,7 @@ public class MyDbContext : DbContext
         modelBuilder.Entity<OrderDetails>(e =>
         {
             e.ToTable("OrderDetails");
-            e.HasKey(row => new {row.OrderId, row.ProductId});
+            e.HasKey(row => new { row.OrderId, row.ProductId });
             e.HasOne(row => row.Order).WithMany(row => row._orderDetails).HasForeignKey(row => row.OrderId);
             e.HasOne(row => row.Product).WithMany(row => row._orderDetails).HasForeignKey(row => row.ProductId);
         });
